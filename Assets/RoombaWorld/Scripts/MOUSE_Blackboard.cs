db8bbc568e9d91603723f9198286047ba63948bf
gere149 <gere.delatorre@gmail.com>
2026-03-08 10:23:36 +0100

    Retoque final

--- Assets/RoombaWorld/Scripts/MOUSE_Blackboard.cs
+++ Assets/RoombaWorld/Scripts/MOUSE_Blackboard.cs
@@ -10,9 +10,6 @@ public class MOUSE_Blackboard : MonoBehaviour
     public float locationReachedRadius = 8f;
     public float exitReachedRadius = 6f;
 
-    public float baseMaxSpeed;
-    public float baseMaxAccel;
-
     void Awake()
     {
         pooPrefab = Resources.Load<GameObject>("POO");
